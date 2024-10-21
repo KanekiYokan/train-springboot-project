113421bf33c41a9554d23b2cbb8bae4c91fa03e1
Tomás Valente <tomassalazarvalente@gmail.com>
2024-10-21 15:41:14 +0100

    New Endpoints in StationController Station Journeys On Station Journeys From Station A to B

--- stations-service/src/main/java/com/mtsds/stations_service/controllers/StationController.java
+++ stations-service/src/main/java/com/mtsds/stations_service/controllers/StationController.java
@@ -1,28 +1,33 @@
 package com.mtsds.stations_service.controllers;
 
+import org.springframework.boot.actuate.autoconfigure.observation.ObservationProperties;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
-
+import java.util.Map;
 
 
 @RestController
 public class StationController {
-
+    TestCPAPI testCPAPI = new TestCPAPI();
     @GetMapping("/")
     public ResponseEntity<String> landing() throws IOException, InterruptedException {
-//        TestCPAPI testCPAPI = new TestCPAPI();
-//        return ResponseEntity.status(HttpStatus.OK).body(teste123.Test());
         return ResponseEntity.status(HttpStatus.OK).body("Station Controller is online.");
     }
 
-    @GetMapping("/stations/")
+    @GetMapping("/stations")
     public ResponseEntity<String> getStations() throws IOException, InterruptedException {
-        TestCPAPI testCPAPI = new TestCPAPI();
         return ResponseEntity.status(HttpStatus.OK).body(testCPAPI.GetStations());
+    }
+    @GetMapping("/stations/{station}")
+    public ResponseEntity<String> getStation(@PathVariable String station) throws IOException, InterruptedException {
+        return ResponseEntity.status(HttpStatus.OK).body(testCPAPI.GetStopover(station));
+    }
+    @PostMapping("/journeys")
+    public ResponseEntity<String> getJourneys(@RequestBody Map<String,String> json) throws IOException, InterruptedException {
+        return ResponseEntity.status(HttpStatus.OK).body(testCPAPI.GetJourneys(json));
     }
 }
 
--- stations-service/src/main/java/com/mtsds/stations_service/controllers/TestCPAPI.java
+++ stations-service/src/main/java/com/mtsds/stations_service/controllers/TestCPAPI.java
@@ -6,6 +6,7 @@ import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.io.IOException;
 import org.json.JSONObject;
+import java.util.Map;
 
 public class TestCPAPI {
     String Token;
@@ -34,7 +35,7 @@ public class TestCPAPI {
         //Chamada para receber estacoes
         HttpClient client = HttpClient.newHttpClient();
         HttpRequest stationRequest = HttpRequest.newBuilder()
-                .uri(URI.create("https://api.cp.pt/cp-api/siv/trains"))
+                .uri(URI.create("https://api.cp.pt/cp-api/siv/stations"))
                 .header("Authorization", "Bearer " + Token)
                 .build();
         HttpResponse<String> stationResponse = client.send(stationRequest, HttpResponse.BodyHandlers.ofString());
@@ -46,25 +47,53 @@ public class TestCPAPI {
 //            Station[] stationArray = objectMapper.readValue(stationResponse.body(), Station[].class);
 //            return stationArray;
     }
-
-    public void GetTrips() throws IOException, InterruptedException {
+    public String GetStopover(String station) throws IOException, InterruptedException {
         if (this.Token == null) {
             GetToken();
         }
+        //Chamada para receber estacoes
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest stopoverRequest = HttpRequest.newBuilder()
+                .uri(URI.create("https://api.cp.pt/cp-api/siv/stations/"+station+"/timetable/"+"2024-11-21")) //TODO alterar fixed date
+                .header("Authorization", "Bearer " + Token)
+                .build();
+        HttpResponse<String> stopoverResponse = client.send(stopoverRequest, HttpResponse.BodyHandlers.ofString());
+
+        return stopoverResponse.body();
+        //Tratamento das estacoes (atencao que e preciso haver exatamente os mesmo campos que vem da chamada da api :shrug:)
+        //mudar return para Station[]
+//        ObjectMapper objectMapper = new ObjectMapper();
+//            Station[] stationArray = objectMapper.readValue(stationResponse.body(), Station[].class);
+//            return stationArray;
+    }
+
+    public String GetJourneys(Map<String,String> json) throws IOException, InterruptedException {
+        if (this.Token == null) {
+            GetToken();
+        }
+        String StartStation = json.get("startStation");
+        String EndStation =json.get("endStation");
+            String Date = json.get("date");
+        if (Date == null){
+            System.out.println(java.time.LocalDate.now().toString());
+            Date = java.time.LocalDate.now().toString();
+        }
         //Chamada para receber uma viagem do ponto A a B no dia X(pontos forcados mas no futuro nao)
         // 94-1008 sao bento
+        // 94-2006 campanha
         // 94-29157 braga
         //A e B tem de ser o id que vem do json (na classe Station o Code penso eu)
 //        String TripString = "{\"departureStationCode\": \"94-1008\",\"arrivalStationCode\": 94-29157,\"classes\": [1, 2],\"searchType\": 3,\"travelDate\": 2024-11-19,\"returnDate\": null,\"timeLimit\": null}";
         HttpClient client = HttpClient.newHttpClient();
-        String TripString = "{\"departureStationCode\": \"94-1008\",\"arrivalStationCode\":\"94-29157\",\"travelDate\":\"2024-11-20\"}";
+        String JourneyString = "{\"departureStationCode\": \""+StartStation+"\",\"arrivalStationCode\":\""+EndStation+"\",\"travelDate\":\""+Date+"\"}";
         HttpRequest tripRequest = HttpRequest.newBuilder()
                 .uri(URI.create("https://api.cp.pt/cp-api/siv/travel/search"))
                 .header("Authorization", "Bearer " + Token)
-                .POST(HttpRequest.BodyPublishers.ofString(TripString))
+                .POST(HttpRequest.BodyPublishers.ofString(JourneyString))
                 .build();
-        HttpResponse<String> tripResponse = client.send(tripRequest, HttpResponse.BodyHandlers.ofString());
-        System.out.println(tripResponse.body());
+        HttpResponse<String> journeyResponse = client.send(tripRequest, HttpResponse.BodyHandlers.ofString());
+//        System.out.println(tripResponse.body());
+        return journeyResponse.body();
     }
 }
 
